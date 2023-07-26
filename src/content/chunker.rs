31750fda389e2bae01f964aa29d7294decb0d920
Azim Muradov <azim.muradov.dev@gmail.com>
2023-07-26 21:32:36 +0300

    Update benchmarks, add compression tests, add FSC

--- src/content/chunker.rs
+++ src/content/chunker.rs
@@ -1,9 +1,9 @@
-use fastcdc::v2020::FastCDC;
 use std::cmp::min;
-use std::fmt::{self, Debug};
+use std::fmt::{self, Debug, Display, Formatter};
 use std::io::{Result as IoResult, Seek, SeekFrom, Write};
 use std::ptr;
 
+use fastcdc::v2020::{FastCDC, Normalization};
 use serde::{Deserialize, Serialize};
 
 // taken from pcompress implementation
@@ -13,6 +13,12 @@ const MASK: u64 = 0x00ff_ffff_ffffu64;
 const MIN_SIZE: usize = 16 * 1024; // minimal chunk size, 16k
 const AVG_SIZE: usize = 32 * 1024; // average chunk size, 32k
 const MAX_SIZE: usize = 64 * 1024; // maximum chunk size, 64k
+
+const FASTCDC_MIN_SIZE: usize = 2 * 1024;
+const FASTCDC_AVG_SIZE: usize = 2 * 1024;
+const FASTCDC_MAX_SIZE: usize = 64 * 1024;
+
+const FIXED_SIZE: usize = 4 * 1024; // maximum chunk size, 64k
 
 // Irreducible polynomial for Rabin modulus, from pcompress
 const FP_POLY: u64 = 0xbfe6_b8a5_bf37_8d83u64;
@@ -30,6 +36,13 @@ const WIN_SLIDE_POS: usize = MIN_SIZE - WIN_SLIDE_OFFSET;
 
 // writer buffer length
 const WTR_BUF_LEN: usize = 8 * MAX_SIZE;
+
+const LVLS: [Normalization; 4] = [
+    Normalization::Level0,
+    Normalization::Level1,
+    Normalization::Level2,
+    Normalization::Level3,
+];
 
 /// Pre-calculated chunker parameters
 #[derive(Clone, Deserialize, Serialize)]
@@ -224,10 +237,13 @@ impl<W: Write + Seek> Seek for Chunker<W> {
     }
 }
 
-
 /// Chunker
 pub struct FastCdcChunker<W: Write + Seek> {
     dst: W, // destination writer
+    min_size: usize,
+    avg_size: usize,
+    max_size: usize,
+    lvl: Normalization,
     pos: usize,
     chunk_len: usize,
     buf_clen: usize, // current length
@@ -242,8 +258,36 @@ impl<W: Write + Seek> FastCdcChunker<W> {
 
         FastCdcChunker {
             dst,
-            pos: MIN_SIZE,
-            chunk_len: MIN_SIZE,
+            min_size: FASTCDC_MIN_SIZE,
+            avg_size: FASTCDC_AVG_SIZE,
+            max_size: FASTCDC_MAX_SIZE,
+            lvl: Normalization::Level0,
+            pos: FASTCDC_MIN_SIZE,
+            chunk_len: FASTCDC_MIN_SIZE,
+            buf_clen: 0,
+            roll_hash: 0,
+            buf,
+        }
+    }
+
+    pub fn fine_tuned(
+        dst: W,
+        min_size: usize,
+        avg_size: usize,
+        max_size: usize,
+        lvl: Normalization,
+    ) -> Self {
+        let mut buf = vec![0u8; WTR_BUF_LEN];
+        buf.shrink_to_fit();
+
+        FastCdcChunker {
+            dst,
+            min_size,
+            avg_size,
+            max_size,
+            lvl,
+            pos: min_size,
+            chunk_len: min_size,
             buf_clen: 0,
             roll_hash: 0,
             buf,
@@ -271,13 +315,19 @@ impl<W: Write + Seek> Write for FastCdcChunker<W> {
         self.buf_clen += in_len;
 
         while self.pos < self.buf_clen {
-            self.pos -= MIN_SIZE;
+            self.pos -= self.min_size;
 
-            let (hash, cut_point) = FastCDC::new(
+            let (hash, cut_point) = FastCDC::with_level(
                 &*self.buf,
-                MIN_SIZE as u32,
-                AVG_SIZE as u32,
-                MAX_SIZE as u32,
+                self.min_size as u32,
+                self.avg_size as u32,
+                self.max_size as u32,
+                match self.lvl {
+                    Normalization::Level0 => Normalization::Level0,
+                    Normalization::Level1 => Normalization::Level1,
+                    Normalization::Level2 => Normalization::Level2,
+                    Normalization::Level3 => Normalization::Level3,
+                },
             )
             .cut(self.pos, self.buf_clen - self.pos);
 
@@ -293,7 +343,7 @@ impl<W: Write + Seek> Write for FastCdcChunker<W> {
 
             // not enough space in buffer, copy remaining to
             // the head of buffer and reset buf position
-            if self.pos + MAX_SIZE >= WTR_BUF_LEN {
+            if self.pos + self.max_size >= WTR_BUF_LEN {
                 let left_len = self.buf_clen - self.pos;
                 unsafe {
                     ptr::copy::<u8>(
@@ -307,8 +357,8 @@ impl<W: Write + Seek> Write for FastCdcChunker<W> {
             }
 
             // jump to next start sliding position
-            self.pos += MIN_SIZE;
-            self.chunk_len = MIN_SIZE;
+            self.pos += self.min_size;
+            self.chunk_len = self.min_size;
         }
 
         Ok(in_len)
@@ -323,8 +373,8 @@ impl<W: Write + Seek> Write for FastCdcChunker<W> {
         }
 
         // reset chunker
-        self.pos = MIN_SIZE;
-        self.chunk_len = MIN_SIZE;
+        self.pos = self.min_size;
+        self.chunk_len = self.min_size;
         self.buf_clen = 0;
         self.roll_hash = 0;
 
@@ -344,18 +394,136 @@ impl<W: Write + Seek> Seek for FastCdcChunker<W> {
     }
 }
 
+/// Chunker
+pub struct FixedSizeChunker<W: Write + Seek> {
+    dst: W, // destination writer
+    chunk_size: usize,
+    pos: usize,
+    chunk_len: usize,
+    buf_clen: usize, // current length
+    buf: Vec<u8>, // chunker buffer, fixed size: WTR_BUF_LEN
+}
+
+impl<W: Write + Seek> FixedSizeChunker<W> {
+    pub fn new(dst: W) -> Self {
+        let mut buf = vec![0u8; WTR_BUF_LEN];
+        buf.shrink_to_fit();
+
+        FixedSizeChunker {
+            dst,
+            chunk_size: FIXED_SIZE,
+            pos: FIXED_SIZE,
+            chunk_len: FIXED_SIZE,
+            buf_clen: 0,
+            buf,
+        }
+    }
+
+    pub fn fine_tuned(dst: W, chunk_size: usize) -> Self {
+        let mut buf = vec![0u8; WTR_BUF_LEN];
+        buf.shrink_to_fit();
+
+        FixedSizeChunker {
+            dst,
+            chunk_size,
+            pos: chunk_size,
+            chunk_len: chunk_size,
+            buf_clen: 0,
+            buf,
+        }
+    }
+
+    pub fn into_inner(mut self) -> IoResult<W> {
+        self.flush()?;
+        Ok(self.dst)
+    }
+}
+
+impl<W: Write + Seek> Write for FixedSizeChunker<W> {
+    // consume bytes stream, output chunks
+    fn write(&mut self, buf: &[u8]) -> IoResult<usize> {
+        if buf.is_empty() {
+            return Ok(0);
+        }
+
+        // copy source data into chunker buffer
+        let in_len = min(WTR_BUF_LEN - self.buf_clen, buf.len());
+        assert!(in_len > 0);
+        self.buf[self.buf_clen..self.buf_clen + in_len]
+            .copy_from_slice(&buf[..in_len]);
+        self.buf_clen += in_len;
+
+        while self.pos < self.buf_clen {
+            // write the chunk to destination writer,
+            // ensure it is consumed in whole
+            let p = self.pos - self.chunk_len;
+            let written = self.dst.write(&self.buf[p..self.pos])?;
+            assert_eq!(written, self.chunk_len);
+
+            // not enough space in buffer, copy remaining to
+            // the head of buffer and reset buf position
+            if self.pos + self.chunk_size >= WTR_BUF_LEN {
+                let left_len = self.buf_clen - self.pos;
+                unsafe {
+                    ptr::copy::<u8>(
+                        self.buf[self.pos..].as_ptr(),
+                        self.buf.as_mut_ptr(),
+                        left_len,
+                    );
+                }
+                self.buf_clen = left_len;
+                self.pos = 0;
+            }
+
+            // jump to next start sliding position
+            self.pos += self.chunk_size;
+            self.chunk_len = self.chunk_size;
+        }
+
+        Ok(in_len)
+    }
+
+    fn flush(&mut self) -> IoResult<()> {
+        // flush remaining data to destination
+        let p = self.pos - self.chunk_len;
+        if p < self.buf_clen {
+            self.chunk_len = self.buf_clen - p;
+            let _ = self.dst.write(&self.buf[p..(p + self.chunk_len)])?;
+        }
+
+        // reset chunker
+        self.pos = self.chunk_size;
+        self.chunk_len = self.chunk_size;
+        self.buf_clen = 0;
+
+        self.dst.flush()
+    }
+}
+
+impl<W: Write + Seek> Debug for FixedSizeChunker<W> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Chunker()")
+    }
+}
+
+impl<W: Write + Seek> Seek for FixedSizeChunker<W> {
+    fn seek(&mut self, pos: SeekFrom) -> IoResult<u64> {
+        self.dst.seek(pos)
+    }
+}
 
 #[cfg(test)]
 mod tests {
+    use std::collections::HashMap;
     use std::io::{copy, Cursor, Result as IoResult, Seek, SeekFrom, Write};
-    use std::time::Instant;
+    use std::iter::FromIterator;
+    use test::Bencher;
+
+    use crate::base::crypto::{Crypto, Hash, RandomSeed, RANDOM_SEED_SIZE};
+    use crate::base::init_env;
+    use crate::content::chunk::Chunk;
 
     use super::*;
-    use crate::base::crypto::{Crypto, RandomSeed, RANDOM_SEED_SIZE};
-    use crate::base::init_env;
-    use crate::base::utils::speed_str;
-    use crate::content::chunk::Chunk;
-    use test::Bencher;
 
     #[derive(Debug)]
     struct Sinker {
@@ -434,6 +602,26 @@ mod tests {
     }
 
     #[bench]
+    fn fsc_chunker_perf(b: &mut Bencher) {
+        b.iter(|| {
+            init_env();
+
+            // perpare test data
+            const DATA_LEN: usize = 10 * 1024 * 1024;
+            let mut data = vec![0u8; DATA_LEN];
+            let seed = RandomSeed::from(&[0u8; RANDOM_SEED_SIZE]);
+            Crypto::random_buf_deterministic(&mut data, &seed);
+            let mut cur = Cursor::new(data);
+            let sinker = VoidSinker {};
+
+            // test chunker performance
+            let mut ckr = FixedSizeChunker::new(sinker);
+            copy(&mut cur, &mut ckr).unwrap();
+            ckr.flush().unwrap();
+        });
+    }
+
+    #[bench]
     fn chunker_perf(b: &mut Bencher) {
         b.iter(|| {
             init_env();
@@ -472,5 +660,175 @@ mod tests {
             copy(&mut cur, &mut ckr).unwrap();
             ckr.flush().unwrap();
         });
+    }
+
+    #[test]
+    fn fsc_chunker_compression_all() {
+        for chunk_size in KB_ALL {
+            fsc_chunker_compression(LNX, chunk_size);
+        }
+    }
+
+    #[test]
+    fn chunker_compression_all() {
+        chunker_compression(LNX);
+    }
+
+    #[test]
+    fn fastcdc_chunker_compression_all() {
+        for min_size in KB_SMALL {
+            for avg_size in KB_SMALL.iter().filter(|kb| **kb >= min_size) {
+                for max_size in KB_ALL.iter().filter(|kb| **kb > *avg_size) {
+                    for lvl in LVLS {
+                        fastcdc_chunker_compression(
+                            LNX, min_size, *avg_size, *max_size, lvl,
+                        );
+                    }
+                }
+            }
+        }
+    }
+
+    const LNX: &str = "/home/graywolf/dev/learn/university/summer-school-2023/project/data-sets/linux-6.4.5-dups.tar";
+
+    const KB_2: usize = 2 * 1024;
+    const KB_4: usize = 4 * 1024;
+    const KB_8: usize = 8 * 1024;
+    const KB_16: usize = 16 * 1024;
+    const KB_32: usize = 32 * 1024;
+    const KB_64: usize = 64 * 1024;
+
+    const KB_SMALL: [usize; 3] = [KB_2, KB_4, KB_8];
+    const KB_ALL: [usize; 6] = [KB_2, KB_4, KB_8, KB_16, KB_32, KB_64];
+
+    fn fsc_chunker_compression(path: &str, chunk_size: usize) {
+        let vec = std::fs::read(path).unwrap();
+
+        init_env();
+
+        let mut sinker = Sinker {
+            len: 0,
+            chks: Vec::new(),
+        };
+
+        let mut cur = Cursor::new(vec.clone());
+        let mut ckr = FixedSizeChunker::fine_tuned(&mut sinker, chunk_size);
+        copy(&mut cur, &mut ckr).unwrap();
+        ckr.flush().unwrap();
+
+        let chks_cnt = sinker.chks.len();
+        let chks_map: HashMap<Hash, usize> = HashMap::from_iter(
+            sinker.chks.into_iter().map(|Chunk { pos, len, .. }| {
+                (Crypto::hash(&vec[pos..(pos + len)]), len)
+            }),
+        );
+
+        println!("FSC chunks: {} / {}", chks_map.len(), chks_cnt);
+        println!(
+            "FSC bytes: {} / {}",
+            chks_map.iter().map(|(a, b)| b).sum::<usize>(),
+            vec.len()
+        );
+    }
+
+    fn chunker_compression(path: &str) {
+        let vec = std::fs::read(path).unwrap();
+
+        init_env();
+
+        let params = ChunkerParams::new();
+
+        let mut sinker = Sinker {
+            len: 0,
+            chks: Vec::new(),
+        };
+
+        let mut cur = Cursor::new(vec.clone());
+        let mut ckr = Chunker::new(params, &mut sinker);
+        copy(&mut cur, &mut ckr).unwrap();
+        ckr.flush().unwrap();
+
+        let chks_cnt = sinker.chks.len();
+        let chks_map: HashMap<Hash, usize> = HashMap::from_iter(
+            sinker.chks.into_iter().map(|Chunk { pos, len, .. }| {
+                (Crypto::hash(&vec[pos..(pos + len)]), len)
+            }),
+        );
+
+        println!("Rabin-based CDC chunks: {} / {}", chks_map.len(), chks_cnt);
+        println!(
+            "Rabin-based CDC bytes: {} / {}",
+            chks_map.iter().map(|(a, b)| b).sum::<usize>(),
+            vec.len()
+        );
+    }
+
+    fn fastcdc_chunker_compression(
+        path: &str,
+        min_size: usize,
+        avg_size: usize,
+        max_size: usize,
+        normalization_lvl: Normalization,
+    ) {
+        let vec = std::fs::read(path).unwrap();
+
+        init_env();
+
+        let mut sinker = Sinker {
+            len: 0,
+            chks: Vec::new(),
+        };
+
+        let mut cur = Cursor::new(vec.clone());
+        let mut ckr = FastCdcChunker::fine_tuned(
+            &mut sinker,
+            min_size,
+            avg_size,
+            max_size,
+            match normalization_lvl {
+                Normalization::Level0 => Normalization::Level0,
+                Normalization::Level1 => Normalization::Level1,
+                Normalization::Level2 => Normalization::Level2,
+                Normalization::Level3 => Normalization::Level3,
+            },
+        );
+        copy(&mut cur, &mut ckr).unwrap();
+        ckr.flush().unwrap();
+
+        let chks_cnt = sinker.chks.len();
+        let chks_map: HashMap<Hash, usize> = HashMap::from_iter(
+            sinker.chks.into_iter().map(|Chunk { pos, len, .. }| {
+                (Crypto::hash(&vec[pos..(pos + len)]), len)
+            }),
+        );
+
+        println!(
+            "FastCDC ({}|{}|{} - {}) chunks: {} / {}",
+            min_size,
+            avg_size,
+            max_size,
+            match normalization_lvl {
+                Normalization::Level0 => 0,
+                Normalization::Level1 => 1,
+                Normalization::Level2 => 2,
+                Normalization::Level3 => 3,
+            },
+            chks_map.len(),
+            chks_cnt
+        );
+        println!(
+            "FastCDC ({}|{}|{} - {}) bytes: {} / {}",
+            min_size,
+            avg_size,
+            max_size,
+            match normalization_lvl {
+                Normalization::Level0 => 0,
+                Normalization::Level1 => 1,
+                Normalization::Level2 => 2,
+                Normalization::Level3 => 3,
+            },
+            chks_map.iter().map(|(a, b)| b).sum::<usize>(),
+            vec.len()
+        );
     }
 }
